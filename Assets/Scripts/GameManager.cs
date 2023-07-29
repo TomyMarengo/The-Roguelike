ecdd45288e830c45c1f7d434cf2d8d68d4cc9910
TomyMarengo <marengotomas@gmail.com>
2023-07-29 18:32:59 -0300

    UI: Pause Game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        Gameplay,
+        Paused,
+        GameOver
+    }
+
+    public GameState currentState;
+    public GameState previousState;
+
+    [Header("UI")]
+    public GameObject pauseScreen;
+    
+    private void Awake() {
+        DisableScreens();
+    }
+    
+    void Update()
+    {
+        switch (currentState)
+        {
+            case GameState.Gameplay:
+                CheckForPauseAndResume();
+                break;
+            case GameState.Paused:
+                CheckForPauseAndResume();
+                break;
+            case GameState.GameOver:
+                break;
+            default:
+                Debug.Log("Invalid Current State");
+                break;
+        }
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+    }
+
+    public void PauseGame()
+    {
+        if(currentState != GameState.Paused) {
+            previousState = currentState;
+            ChangeState(GameState.Paused);
+            Time.timeScale = 0f;
+            pauseScreen.SetActive(true);
+        } 
+    }
+
+    public void ResumeGame()
+    {
+        if(currentState == GameState.Paused) {
+            ChangeState(previousState);
+            Time.timeScale = 1f;
+            pauseScreen.SetActive(false);
+        }
+    }
+
+    void CheckForPauseAndResume()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(currentState == GameState.Paused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    void DisableScreens()
+    {
+        pauseScreen.SetActive(false);
+    }
+
+}
